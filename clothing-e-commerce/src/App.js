9a6052f157fbe31ed3244949c7c877cd4e64672f
Patryck Brenner <patryck_brenner@outlook.com>
2021-07-07 00:03:37 +0100

    Implementing redirect component from react-router-dom to redirect to home if user is signed in. Implemented user types to avoid spelling mistakes

--- clothing-e-commerce/src/App.js
+++ clothing-e-commerce/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import "./App.css";
@@ -9,31 +9,31 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser} from './redux/user/user.actions';
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-
   //use it to avoid memory leaks of authentication. Set authentication to null
   unsubscribeFromAuth = null;
 
   //when a user log in, the state will change to the name of the user
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      if (userAuth) {//if the authorization is successful
+      if (userAuth) {
+        //if the authorization is successful
         //get the reference from the createUser method
         const userRef = await createUserProfileDocument(userAuth); //if the user is not registered, create a new userRef doc
 
         //collecting the data from database to the application, by setting state to the user properties
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser ({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
         });
-
-      } else { //set state of the current user to null again
+      } else {
+        //set state of the current user to null again
         setCurrentUser(userAuth);
       }
     });
@@ -57,15 +57,29 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact={false} path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() => // if the user is logged in, it redirects and does not allow user to go to signin page 
+              this.props.currentUser ? (
+                <Redirect to="/" /> // redirects to homepage
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
 });
 
-export default connect(null, mapDispatchToProps )(App);
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- clothing-e-commerce/src/redux/user/user.reducer.jsx
+++ clothing-e-commerce/src/redux/user/user.reducer.jsx
@@ -1,3 +1,4 @@
+import {UserActionTypes} from './user.types';
 
 const INITIAL_STATE = {
     currentUser : null
@@ -7,7 +8,7 @@ const INITIAL_STATE = {
 const userReducer = (state = INITIAL_STATE, action) => {
     //can use if statement as well
     switch(action.type) { //it will check every action's type
-        case 'SET_CURRENT_USER': //in case of an "SET_CURRENT_USER"
+        case UserActionTypes.SET_CURRENT_USER: //in case of an "SET_CURRENT_USER"
             return {
                 ...state, //we don't mind about the other states...
                 currentUser: action.payload //set the currentUser to whatever payload is passed in
