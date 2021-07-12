43241e869de5d46fe9865c309cf790765e34ebac
Patryck Brenner <patryck_brenner@outlook.com>
2021-07-12 21:35:51 +0100

    Implemented addItem adn removeItem functionalities to checkout item component

--- clothing-e-commerce/src/components/checkout-item/checkout-item.component.jsx
+++ clothing-e-commerce/src/components/checkout-item/checkout-item.component.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.actions";
 
-import './checkout-item.styles.scss';
+import "./checkout-item.styles.scss";
 
 //when this component is called, uses "cartItem" as prop to populate the component
-const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}}) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt='item'/>
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005; {/*codig to display the X "https://www.w3schools.com/charsets/ref_utf_dingbats.asp"*/ }</div>
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;{" "}
+        {/*codig to display the X "https://www.w3schools.com/charsets/ref_utf_dingbats.asp"*/}
+      </div>
     </div>
-)
+  );
+};
 
-export default CheckoutItem;
+//update the state of item in the function "clearItemFromCart" from reducer
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- clothing-e-commerce/src/redux/cart/cart.actions.js
+++ clothing-e-commerce/src/redux/cart/cart.actions.js
@@ -9,3 +9,13 @@ export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM, //just informing reducer that this is an ADD_ITEM type, so this action will do...
   payload: item, //in this case the action will pass in an object item as payload, but it could be anything
 });
+
+export const removeItem = item => ({
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+});
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
