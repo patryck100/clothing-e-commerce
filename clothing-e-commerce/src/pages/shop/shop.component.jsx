63bc2adb39cd24ccb665c2b6351d75415e8e82eb
Patryck Brenner <patryck_brenner@outlook.com>
2021-07-13 12:36:05 +0100

    Implemented collection-item to retrive data from shop according to collection, for example hats retrieves all the hats from database. Also transformed shop data from array to objects, to improve performance when retriving data, since find() function can take too long when the data grows up in a worst case cenario

--- clothing-e-commerce/src/pages/shop/shop.component.jsx
+++ clothing-e-commerce/src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
+//gives access to match, location and history
+import { Route } from "react-router-dom";
 
-const ShopPage = ({ collections }) => (
+import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return(
   <div className="shop-page">
-      <CollectionsOverview/>
+      <Route exact path={`${match.path}` /*same as /shop:category*/} component={CollectionsOverview}/>
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
   </div>
-);
+)};
 
 
 export default ShopPage;
--- clothing-e-commerce/src/redux/shop/shop.selectors.js
+++ clothing-e-commerce/src/redux/shop/shop.selectors.js
@@ -1,8 +1,34 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
 
-const selectShop = state => state.shop;
+//because the url parameter is a string and the id is a number, a map was created to match both
+//but after transforming the data in objects instead of arrays, it solved the problem so it is not needed anymore
+/*const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+}; */
+
+const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  (shop) => shop.collections
+);
+
+
+/*By wrapping this function with memoize, we're saying that whenever this function 
+gets called and receives collectionUrlParam, I want to memoize the return of this 
+function (in this case we return a selector). If this function gets called again with
+the same collectionUrlParam, don't rerun this function because we'll return the same 
+value as last time, which we've memoized so just return the selector that's been stored. */
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections[collectionUrlParam]    
+  /*collections.find( //find collection.id matching the url parameter from the collection_id_map **removed to increase performance, find() can get slow if the data is too big
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )*/
+  )
 );
