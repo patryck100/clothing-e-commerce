d7e8eb2b93ceb54706da3061d71258a41b4138ba
Patryck Brenner <patryck_brenner@outlook.com>
2021-07-14 20:32:32 +0100

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,17 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 //this is just to make it scalable, to increase with the input
-const middlewares = [logger];
+const middlewares = [];
+
+//it can be development, production or test
+//when run yarn start it sets to development, whereas "build" set it to production because it is going to be deployed
+//this way it will only push the logger information through the middleware when the application is in development mode
+if(process.env.NODE_ENV === 'development') { 
+    middlewares.push(logger);
+}
 
 //...middlewares basically spread in the "applyMiddleware()" function each logger as individual argument
+//when actions come through being dispached by any action, it updates the reducer and the middlewares intercept the info and do something
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 //allows the browser to catch or store data depending on configuration option
